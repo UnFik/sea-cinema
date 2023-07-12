30895726aaca0c4af1b7cefcac24bf2b2388ae57
UnFik <fikriilhamarifin_1313621027@mhs.unj.ac.id>
2023-07-12 11:56:48 +0700

    add lib

--- app/api/movies/[id]/route.ts
+++ app/api/movies/[id]/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import prismadb from "@/libs/prismadb";
 
 export async function GET(
   request: Request,
@@ -15,6 +16,6 @@ export async function GET(
 
     return NextResponse.json({ movie });
   } catch (error) {
-    return new NextResponse("Title Doesnt Exist", { status: 400 });
+    return new NextResponse(`Title Doesnt Exist ${error}`, { status: 400 });
   }
 }
